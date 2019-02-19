2b0554e4b0031d0b1cf21f82cf948c168effedbf
Artur-torba <44593868+Artur-torba@users.noreply.github.com>
2019-02-19 11:31:22 +0400

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,15 @@
+Проект собран в Gulp. Исходные данные  находятся в папке SRC.Конечные данные минимизированны и  находятся в папке dest.
+
+чтобы изменить стили необходимо поменят тег  
+<link rel="stylesheet" href="dest/css/all.css"> на <link rel="stylesheet" href="src/css/index.css">
+
+
+для изменения js необходимо 
+
+тег <script src="dest/js/all.js"></script> поменять на <script src="src/js/main.js.js"></script>
+(тег <script> находится передзакрывающим тегом </body>.
+
+мой контактный телефон +7 982 797 6757
+мой email artur.torba@mail.ru
+
+моя страничка - резюме https://artur-torba.github.io/
---
+++ src/js/main.js.js
@@ -0,0 +1,100 @@
+window.onload = function(){
+	document.querySelector('.tabs-header').addEventListener('click', fTabs);
+	function fTabs(event){
+		console.log(event);
+		if (event.target.className == 'tab-h') {
+			var dataTab = event.target.getAttribute('data-tab');
+			//dataTab -номер вкладки, которую нужноотобразить.
+			var tabBody = document.getElementsByClassName('tab-b');
+			// Отключаю класс active
+			var tabH = document.getElementsByClassName('tab-h');
+			for(var i=0; i<tabH.length; i++){
+				tabH[i].classList.remove('active');
+			}
+			event.target.classList.add('active');
+			//все вкладки с содержимым
+			for (var i=0; i<tabBody.length; i++){
+				if(dataTab==i){ 
+					tabBody[i].style.display = 'block';
+				}
+				else{
+					tabBody[i].style.display = 'none';
+				}
+			}
+		}
+		
+	}
+
+
+var music = document.getElementById('music');
+music.onclick = function(){
+	this.className = 'hide';
+}
+
+var computer = document.getElementById('computer');
+computer.onclick = function(){
+	this.className = 'hide';
+}
+var radio = document.getElementById('radio');
+radio.onclick = function(){
+	this.className = 'hide';
+}
+var dance = document.getElementById('dance');
+dance.onclick = function(){
+	this.className = 'hide';
+}
+var pussy = document.getElementById('pussy');
+pussy.onclick = function(){
+	this.className = 'hide';
+}
+
+
+
+var addfriend = document.getElementById('addfriend');
+addfriend.onclick = function(){
+var select = document.getElementById('select');
+var option = select.options[select.selectedIndex].value;
+if(option == 'music'){
+	music.classList.remove('hide')
+}
+if(option == 'computers'){
+	computer.classList.remove('hide')
+}
+else if(option == 'radio'){
+	radio.classList.remove('hide')
+}
+else if(option == 'dance'){
+	dance.classList.remove('hide')
+}
+else if(option == 'pussy'){
+	pussy.classList.remove('hide')
+}
+ }
+ // var username = document.getElementById('username');
+ // username.onclick = function(){
+ // 	this.innerHTML =  '<input  id="changename" type="text" autofocus>'
+ //    var changename = document.getElementById('changename');
+     
+
+ // }
+
+ var input1 = document.getElementById('1');
+ input1.value = localStorage.getItem('dataForMyInput1') || 'Виталя Гора';
+ input1.oninput = function(){
+localStorage.setItem('dataForMyInput1', input1.value);
+}
+var input2 = document.getElementById('2');
+ input2.value = localStorage.getItem('dataForMyInput2') || '+7 (982) 797 67 57';
+ input2.oninput = function(){
+localStorage.setItem('dataForMyInput2', input2.value);
+}
+var input3 = document.getElementById('3');
+ input3.value = localStorage.getItem('dataForMyInput3') || 'artur.torba@mail.ru';
+ input3.oninput = function(){
+localStorage.setItem('dataForMyInput3', input3.value);
+}
+
+}
+
+	
+
